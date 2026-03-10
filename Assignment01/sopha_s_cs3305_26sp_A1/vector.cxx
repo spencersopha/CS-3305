04e75c163b6a9e72b01c06998b3318d4f55de41f
Spencer Sopha <spencer.sopha@gmail.com>
2026-03-09 22:58:17 -0500

    Creating abstract data types to represent mathematical objects for matrix computations

--- Assignment01/sopha_s_cs3305_26sp_A1/vector.cxx
+++ Assignment01/sopha_s_cs3305_26sp_A1/vector.cxx
@@ -4,13 +4,16 @@
 namespace spencer {
 
 // Constructor
-vector::vector(double x1, double x2, bool is_row)
-    : x1(x1), x2(x2), is_row(is_row) {}
+vector::vector(double x1, double x2, bool is_row) {
+    x1 = x1;
+    x2 = x2;
+    is_row = is_row;
+}
 
 // Getters
-double vector::get_x1() const { return x1; }
-double vector::get_x2() const { return x2; }
-bool vector::get_is_row() const { return is_row; }
+double vector::get_x1() { return x1; }
+double vector::get_x2() { return x2; }
+bool vector::get_is_row() { return is_row; }
 
 // Setters
 void vector::set_x1(double val) { x1 = val; }
@@ -18,12 +21,12 @@ void vector::set_x2(double val) { x2 = val; }
 void vector::set_is_row(bool val) { is_row = val; }
 
 // Transpose: column -> row, row -> column
-vector vector::transpose() const {
+vector vector::transpose() {
     return vector(x1, x2, !is_row);
 }
 
 // Print
-void vector::print() const {
+void vector::print() {
     if (is_row) {
         std::cout << "[" << x1 << " " << x2 << "]" << std::endl;
     } else {
@@ -33,51 +36,51 @@ void vector::print() const {
 }
 
 // vector + vector
-vector operator+(const vector& lhs, const vector& rhs) {
+vector operator+(vector lhs, vector rhs) {
     return vector(lhs.get_x1() + rhs.get_x1(), 
                   lhs.get_x2() + rhs.get_x2(),
                   lhs.get_is_row());
 }
 
 // vector - vector
-vector operator-(const vector& lhs, const vector& rhs) {
+vector operator-(vector lhs, vector rhs) {
     return vector(lhs.get_x1() - rhs.get_x1(), 
                   lhs.get_x2() - rhs.get_x2(),
                   lhs.get_is_row());
 }
 
 // vector * vector
-vector operator*(const vector& lhs, const vector& rhs) {
+vector operator*(vector lhs, vector rhs) {
     return vector(lhs.get_x1() * rhs.get_x1(), 
                   lhs.get_x2() * rhs.get_x2(),
                   lhs.get_is_row());
 }
 
 // vector + scalar
-vector operator+(const vector& v, double k) {
+vector operator+(vector v, double k) {
     return vector(v.get_x1() + k, v.get_x2() + k, v.get_is_row());
 }
 
 // scalar + vector
-vector operator+(double k, const vector& v) {
+vector operator+(double k, vector v) {
     return v + k;
 }
 
 // vector - scalar
-vector operator-(const vector& v, double k) {
+vector operator-(vector v, double k) {
     return vector(v.get_x1() - k, v.get_x2() - k, v.get_is_row());
 }
 // scalar - vector
-vector operator-(double k, const vector& v) {
+vector operator-(double k, vector v) {
     return vector(k - v.get_x1(), k - v.get_x2(), v.get_is_row());
 }
 
 // vector * scalar
-vector operator*(const vector& v, double k) {
+vector operator*(vector v, double k) {
     return vector(v.get_x1() * k, v.get_x2() * k, v.get_is_row());
 }
 // scalar * vector
-vector operator*(double k, const vector& v) {
+vector operator*(double k, vector v) {
     return v * k;
 }
 
