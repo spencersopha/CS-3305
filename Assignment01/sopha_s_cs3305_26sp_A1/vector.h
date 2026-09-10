d3d04013552b26a367a91d0c56885210e4960a95
Spencer Sopha <spencer.sopha@gmail.com>
2026-09-09 21:12:12 -0500

    Refactor vector class and update namespace

--- Assignment01/sopha_s_cs3305_26sp_A1/vector.h
+++ Assignment01/sopha_s_cs3305_26sp_A1/vector.h
@@ -1,58 +1,51 @@
+// Spencer Sopha
+// Buff ID: 1093171
+// DSA - A1
+
 #ifndef VECTOR_H
 #define VECTOR_H
 
-namespace spencer {
+namespace sopha {
+    class vector {
+        private:
+            double x1, x2;
+            bool is_row;    // true = row vector, false = column vector
 
-class vector {
-private:
-    double x1;
-    double x2;
-    bool is_row;    // true = row vector, false = column vector
+        public:
+            // Default constructor with default argument values
+            vector (double x1=0, double x2=0, bool is_row=false);
 
-public:
-    // Default constructor with default argument values
-    vector(double x1 = 1, double x2 = 1, bool is_row = false);
+            // Transpose function to return a new vector with transposed orientation
+            vector transpose();
 
-    // Getters
-    double get_x1();
-    double get_x2();
-    bool get_is_row();
+            // Print function to display the vectors
+            void print();
 
-    // Setters
-    void set_x1(double val);
-    void set_x2(double val);
-    void set_is_row(bool val);
+            // Getters for the vector components
+            double get_x1();
+            double get_x2();
+            bool get_is_row();
 
-    // Transpose
-    vector transpose();
+            // Setters for the vector components
+            void set_x1(double val);
+            void set_x2(double val);
+            void set_is_row(bool val);
+    };
 
-    // Print
-    void print();
-};
+    // Overladed operators for vector addition, subtraction, and multiplication
+    vector operator+(double k, vector v);    // scalar + vector
+    vector operator+(vector v, double k);    // vector + scalar
 
-// Operator overloading: vector + vector
-vector operator+(vector lhs, vector rhs);
-// Operator overloading: vector - vector
-vector operator-(vector lhs, vector rhs);
-// Operator overloading: vector * vector
-vector operator*(vector lhs, vector rhs);
+    vector operator-(double k, vector v);    // scalar - vector
+    vector operator-(vector v, double k);    // vector - scalar
 
+    vector operator*(double k, vector v);    // scalar * vector
+    vector operator*(vector v, double k);    // vector * scalar
 
-// Operator overloading: vector + scalar
-vector operator+(vector v, double k);
-// Operator overloading: scalar + vector
-vector operator+(double k, vector v);
-
-// Operator overloading: vector - scalar
-vector operator-(vector v, double k);
-// Operator overloading: scalar - vector
-vector operator-(double k, vector v);
-
-// Operator overloading: vector * scalar
-vector operator*(vector v, double k);
-// Operator overloading: scalar * vector
-vector operator*(double k, vector v);
-
-}
+    vector operator+(vector v1, vector v2);    // vector + vector
+    vector operator-(vector v1, vector v2);    // vector - vector
+    vector operator*(vector v1, vector v2);    // vector * vector
 
 #endif
+
+}
