c7b5b883f7bde740c2d000b0fe049ee9e9cf0d58
Spencer Sopha <spencer.sopha@gmail.com>
2026-09-09 21:14:10 -0500

    Refactor vector class and update operators

--- Assignment01/sopha_s_cs3305_26sp_A1/vector.cxx
+++ Assignment01/sopha_s_cs3305_26sp_A1/vector.cxx
@@ -1,87 +1,92 @@
+// Spencer Sopha
+// Buff ID: 1093171
+// DSA - A1
+
 #include "vector.h"
 #include <iostream>
 
-namespace spencer {
-
-// Constructor
-vector::vector(double x1, double x2, bool is_row) {
-    x1 = x1;
-    x2 = x2;
-    is_row = is_row;
-}
-
-// Getters
-double vector::get_x1() { return x1; }
-double vector::get_x2() { return x2; }
-bool vector::get_is_row() { return is_row; }
-
-// Setters
-void vector::set_x1(double val) { x1 = val; }
-void vector::set_x2(double val) { x2 = val; }
-void vector::set_is_row(bool val) { is_row = val; }
-
-// Transpose: column -> row, row -> column
-vector vector::transpose() {
-    return vector(x1, x2, !is_row);
-}
-
-// Print
-void vector::print() {
-    if (is_row) {
-        std::cout << "[" << x1 << " " << x2 << "]" << std::endl;
-    } else {
-        std::cout << "[" << x1 << std::endl;
-        std::cout << " " << x2 << "]" << std::endl;
+namespace sopha {
+    // Default construtor
+    vector::vector(double x1, double x2, bool is_row) {
+        x1 = x1;
+        x2 = x2;
+        is_row = is_row;
     }
-}
 
-// vector + vector
-vector operator+(vector lhs, vector rhs) {
-    return vector(lhs.get_x1() + rhs.get_x1(), 
-                  lhs.get_x2() + rhs.get_x2(),
-                  lhs.get_is_row());
-}
+    // Tranpose function: column => row, row => column
+    vector vector::transpose() {
+        return vector(x1, x2, !is_row);
+    }
 
-// vector - vector
-vector operator-(vector lhs, vector rhs) {
-    return vector(lhs.get_x1() - rhs.get_x1(), 
-                  lhs.get_x2() - rhs.get_x2(),
-                  lhs.get_is_row());
-}
+    // Print the vector in row or column format based on is_row
+    void vector::print() {
+        if (!is_row) {
+            std::cout << "[" << x1 << " " << x2 << "]" << "\n";
+        } else {
+            std::cout << "[" << x1 << "\n";
+            std::cout << " " << x2 << "]" << "\n";
+        }
+    }
 
-// vector * vector
-vector operator*(vector lhs, vector rhs) {
-    return vector(lhs.get_x1() * rhs.get_x1(), 
-                  lhs.get_x2() * rhs.get_x2(),
-                  lhs.get_is_row());
-}
+    // Getters
+    double vector::get_x1() { return x1; }
+    double vector::get_x2() { return x2; }
+    bool vector::get_is_row() { return is_row; }
 
-// vector + scalar
-vector operator+(vector v, double k) {
-    return vector(v.get_x1() + k, v.get_x2() + k, v.get_is_row());
-}
+    // Setters
+    void vector::set_x1(double val) { x1 = val; }
+    void vector::set_x2(double val) { x2 = val; }
+    void vector::set_is_row(bool val) { is_row = val; }
 
-// scalar + vector
-vector operator+(double k, vector v) {
-    return v + k;
-}
+    // Overladed operators
+    // scalar + vector
+    vector operator+(double k, vector v) {    
+        return vector(k + v.get_x1(), k + v.get_x2(), v.get_is_row());
+    }
 
-// vector - scalar
-vector operator-(vector v, double k) {
-    return vector(v.get_x1() - k, v.get_x2() - k, v.get_is_row());
-}
-// scalar - vector
-vector operator-(double k, vector v) {
-    return vector(k - v.get_x1(), k - v.get_x2(), v.get_is_row());
-}
+    // vector + scalar
+    vector operator+(vector v, double k) {    
+        return vector(v.get_x1() + k, v.get_x2() + k, v.get_is_row());
+    }
 
-// vector * scalar
-vector operator*(vector v, double k) {
-    return vector(v.get_x1() * k, v.get_x2() * k, v.get_is_row());
-}
-// scalar * vector
-vector operator*(double k, vector v) {
-    return v * k;
-}
+    // scalar - vector
+    vector operator-(double k, vector v) {    
+         return vector(k - v.get_x1(), k - v.get_x2(), v.get_is_row());
+    }
 
+    // vector - scalar
+    vector operator-(vector v, double k) {    
+        return vector(v.get_x1() - k, v.get_x2() - k, v.get_is_row());
+    }
+
+    // scalar * vector
+    vector operator*(double k, vector v) {    
+        return vector(k * v.get_x1(), k * v.get_x2(), v.get_is_row());
+    }
+
+    // vector * scalar
+    vector operator*(vector v, double k) {    
+        return vector(v.get_x1() * k, v.get_x2() * k, v.get_is_row());
+    }
+
+    // vector + vector
+    vector operator+(vector v1, vector v2) {    
+        return vector(v1.get_x1() + v2.get_x1(),
+                      v1.get_x2() + v2.get_x2(),
+                      v1.get_is_row());
+    }
+
+    // vector - vector
+    vector operator-(vector v1, vector v2) {    
+        return vector(v1.get_x1() - v2.get_x1(),
+                      v1.get_x2() - v2.get_x2(),
+                      v1.get_is_row());
+    }
+
+    // vector * vector
+    vector operator*(vector v1, vector v2) {    
+        return vector(v1.get_x1() * v2.get_x1(),
+                      v1.get_x2() * v2.get_x2(),
+                      v1.get_is_row());
+    }
 }
